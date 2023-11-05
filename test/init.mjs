7f7293e012a279e0f210dbef35f32f4728cd3757
Dmitry Sharabin <dmitrysharabin@gmail.com>
2023-11-05 17:52:01 +0100

    Use dynamic import for Bliss

--- test/init.mjs
+++ test/init.mjs
@@ -1,4 +1,3 @@
-import execfile from "./util/execfile.mjs";
 import { JSDOM } from "jsdom";
 import jsep from "../../mavo/lib/jsep.min.js";
 
@@ -13,7 +12,7 @@ globalThis.location = window.location;
 
 // Global dependencies
 globalThis.jsep = jsep; // JSEP
-execfile("../../mavo/lib/bliss.shy.min.js", { ...globalThis }); // Bliss
+await import("../../mavo/lib/bliss.shy.min.js"); // Bliss
 
 // Mock Mavo object
 globalThis.Mavo = {
